96e599231001bbf1aa1156a237ddee0d709e48dd
vishwanath79@outlook.com <vishwanath79@outlook.com>
2015-03-23 21:21:41 -0700

    login

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,18 @@
-pass
+from flask import render_template
+
+from blog import app
+from database import session
+from models import Post
+
+@app.route("/")
+def posts():
+    posts = session.query(Post)
+    posts = posts.order_by(Post.datetime.desc())
+    posts = posts.all()
+    return render_template("posts.html",
+        posts=posts
+    )
+    
+@app.route("/login", methods=["GET"])
+def login_get():
+	return render_template("login.html")
